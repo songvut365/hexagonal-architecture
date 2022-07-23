fd5a81ddb82e635b2aca6ee8cf00709ac4182bda
songvut365 <songvut.nakrong@gmail.com>
2022-07-23 23:39:47 +0700

    Update account repo, service, handler, router

--- main.go
+++ main.go
@@ -21,20 +21,24 @@ func main() {
 	db := config.InitDatabase()
 
 	// Repository
-	customerRepositoryMock := repository.NewCustomerRepositoryMock()
 	customerRepository := repository.NewCustomerRepositoryDB(db)
-	_ = customerRepositoryMock
+	accountRepository := repository.NewAccountRepositoryDB(db)
 
 	// Service
 	customerService := service.NewCustomerService(customerRepository)
+	accountService := service.NewAccountService(accountRepository)
 
 	// Handler
 	customerHandler := handler.NewCustomerHandler(&customerService)
+	accountHandler := handler.NewAccountHandler(accountService)
 
 	// Router
 	router := mux.NewRouter()
 	router.HandleFunc("/customers", customerHandler.GetCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer).Methods(http.MethodGet)
+
+	router.HandleFunc("/accounts/{customerID:[0-9]+}", accountHandler.NewAccount).Methods(http.MethodPost)
+	router.HandleFunc("/accounts/{customerID:[0-9]+}/accounts", accountHandler.GetAccounts).Methods(http.MethodGet)
 
 	// Run server
 	port := fmt.Sprintf(":%v", viper.GetInt("app.port"))
