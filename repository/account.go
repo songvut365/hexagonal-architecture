2342cd22da418ad16049f36bc04ba691e8db1587
songvut365 <songvut.nakrong@gmail.com>
2022-07-23 23:09:10 +0700

    Change amount type to float64

--- repository/account.go
+++ repository/account.go
@@ -1,12 +1,12 @@
 package repository
 
 type Account struct {
-	AccountID   int    `db:"account_id"`
-	CustomerID  int    `db:"customer_id"`
-	OpeningDate string `db:"opening_date"`
-	AccountType string `db:"account_type"`
-	Amount      int    `db:"amount"`
-	Status      int    `db:"status"`
+	AccountID   int     `db:"account_id"`
+	CustomerID  int     `db:"customer_id"`
+	OpeningDate string  `db:"opening_date"`
+	AccountType string  `db:"account_type"`
+	Amount      float64 `db:"amount"`
+	Status      int     `db:"status"`
 }
 
 type AccountRepository interface {
